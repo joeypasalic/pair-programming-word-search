8157be68f12b1ca57a9c676c0a862dc1a0a761b5
josephpasalic <156459437+joeypasalic@users.noreply.github.com>
2024-02-14 18:59:05 -0500

    implemented a nested for loop to iterate through the columns and rows of the 2D array, in order to check if vertical words exist

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,21 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
 
-module.exports = wordSearch
+  for (let column = 0; column < letters[0].length; column++) {
+    let columnWord = '';
+    for (let row = 0; row < letters.length; row++) {
+
+      columnWord += letters[row][column];
+      console.log(columnWord);
+    }
+    if (columnWord.includes(word))
+      return true;
+
+  }
+  return false;
+};
+
+module.exports = wordSearch;
